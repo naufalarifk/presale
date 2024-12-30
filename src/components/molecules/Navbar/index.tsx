3f408e5f7084ee392bf54fe77816e8dc87c252ab
naufalarifk <abnormalchild414@gmail.com>
2024-12-30 16:46:56 +0700

    chore: added custom classes

--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 export const Navbar = () => {
     return (
         <div className='py-8'>
-            <div className="border-[rgba(255, 255, 255, 0.10)] border-[0.5px] mx-auto flex justify-between p-6 w-[90vw] h-20 bg-[rgba(255, 255, 255, 0.05)] bg-opacity-20 backdrop-blur-md rounded-full shadow-xl">
+            <div className="transparent-container mx-auto flex justify-between p-6 w-[90vw] h-20 rounded-full">
                 <div className='flex items-center space-x-2'>
                     <img src="/MbridgeLogo.svg" alt="" srcSet="" />
                     <p>Mbridge28</p>
                 </div>
                 <div className='flex items-center space-x-2'>
-                    <div className='bg-[rgba(255,255,255,0.07)] border-[rgba(255, 255, 255, 0.10)] border-[0.5px] bg-opacity-20 backdrop-blur-md rounded-full p-4'>
+                    <div className='transparent-child rounded-full p-4'>
                         <p>Get Started</p>
                     </div>
                     <List />
